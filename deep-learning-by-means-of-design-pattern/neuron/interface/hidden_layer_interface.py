a8663d87592cf0b139c6742e410ad6bc840e60aa
Chimera <kimera0kimaira@gmail.com>
2016-06-17 02:19:56 +0900

    Update hidden_layer_interface.py

--- deep-learning-by-means-of-design-pattern/neuron/interface/hidden_layer_interface.py
+++ deep-learning-by-means-of-design-pattern/neuron/interface/hidden_layer_interface.py
@@ -11,12 +11,11 @@ class HiddenLayerInterface(metaclass=ABCMeta):
     '''
 
     @abstractmethod
-    def hidden_update_state(self, weight, link_value):
+    def hidden_update_state(self, link_value):
         '''
-        可視層の学習
+        隠れ層の学習
 
         Args:
-            weight:          重み
             link_value:      リンク先による入力値
 
         '''

fa5b7553975a8726f3f07fcaa1ee21699199948c
Chimera <kimera0kimaira@gmail.com>
2016-06-18 15:22:41 +0900

    Update neural_network_graph.py

--- deep-learning-by-means-of-design-pattern/synapse/neural_network_graph.py
+++ deep-learning-by-means-of-design-pattern/synapse/neural_network_graph.py
@@ -27,61 +27,72 @@ class NeuralNetworkGraph(Synapse):
 
         self.__sigmoid_function = SigmoidFunction()
 
-    def back_propagate(self, propagated_list, learning_rate=0.05, momentum_factor=0.1):
+    def back_propagate(
+        self,
+        propagated_list,
+        learning_rate=0.05,
+        momentum_factor=0.1,
+        back_nn_list=None,
+        back_nn_index=0
+    ):
         '''
-        「中間層における最上位層」からそれ以降の中間層、
-        及び入力層までの組み合わせを前提として、
-        フォワードプロパゲーションを実行する
+        再帰的に自己自身を呼び出すことで
+        各層間のバックプロパゲーションを順次実行していく
 
         Args:
             propagated_list:    伝播内容のリスト、出力層ならば目的変数のリストになる
             learning_rate:      学習率
             momentum_factor:    運動量係数
+            back_nn_list:       再帰的に呼び出すニューラルネットワークのオブジェクト
+            back_nn_index:      再帰中に呼び出しているオブジェクトの添え字番号
 
         Returns:
-            伝播内容のリスト
+            伝播内容と活性度のtupleのリスト
         '''
-        diff_list = []
-        if self.__output_layer_flag is False:
+
+        if self.__output_layer_flag is True:
             '''
-            「中間層における最上位層」からそれ以降の中間層、
-            及び入力層までの組み合わせ
+            出力層から「中間層における最上位層」までの組み合わせ
+            一旦外部から入力された教師データと最上位である出力層の出力値との誤差を検出した後に、
+            それを下層となる中間層に伝播する
             '''
             if len(self.deeper_neuron_list) != len(propagated_list):
                 print(len(self.deeper_neuron_list))
                 print(len(propagated_list))
-                raise ValueError()
+                print(propagated_list)
+                raise IndexError()
 
-            for i in range(len(self.shallower_neuron_list)):
-                error = 0.0
-                for j in range(len(self.deeper_neuron_list)):
-                    self.diff_weights_dict.setdefault((i, j), 0.0)
-                    error += propagated_list[j] * self.diff_weights_dict[(i, j)]
-
-                activity = self.shallower_neuron_list[i].activity
+            diff_list = []
+            for j in range(len(self.deeper_neuron_list)):
+                # 教師データリストの諸要素と出力層の各ニューロンの多重度は1:1
+                error = propagated_list[j] - self.deeper_neuron_list[j].activity
+                activity = self.deeper_neuron_list[j].activity
+                # 外部から出力層に入力された場合、
+                # 言うなれば伝播すべき誤差データが無いために、ここで生成する
                 diff_list.append(
                     self.__sigmoid_function.derivative(activity) * error
                 )
         else:
-            '''
-            出力層から「中間層における最上位層」までの組み合わせ
-            '''
-            diff_list = []
-            for i in range(len(self.deeper_neuron_list)):
-                error = propagated_list[i] - self.deeper_neuron_list[i].activity
+            diff_list = propagated_list
 
-                activity = self.deeper_neuron_list[i].activity
-                diff_list.append(
-                    self.__sigmoid_function.derivative(activity) * error
-                )
-
+        back_propagated_list = []
         for i in range(len(self.shallower_neuron_list)):
+            activity = self.shallower_neuron_list[i].activity
+            error = 0.0
             for j in range(len(self.deeper_neuron_list)):
                 self.diff_weights_dict.setdefault((i, j), 0.0)
                 self.__momentum_factor_dict.setdefault((i, j), 0.0)
-                diff = diff_list[j] * self.shallower_neuron_list[i].activity
-                self.diff_weights_dict[(i, j)] += learning_rate * diff + momentum_factor * self.__momentum_factor_dict[(i, j)]
+
+                diff = diff_list[j] * activity
+                momentum = momentum_factor * self.__momentum_factor_dict[(i, j)]
+                self.diff_weights_dict[(i, j)] += (learning_rate * diff) + momentum
                 self.__momentum_factor_dict[(i, j)] = diff
+
+                error += diff_list[j] * self.weights_dict[(i, j)]
+
+            back_propagated_list.append(
+                self.__sigmoid_function.derivative(activity) * error
+            )
 
         # 重みの更新
         self.learn_weights()
@@ -89,4 +100,12 @@ class NeuralNetworkGraph(Synapse):
         [neuron.update_bias(learning_rate) for neuron in self.shallower_neuron_list]
         [neuron.update_bias(learning_rate) for neuron in self.deeper_neuron_list]
 
-        return diff_list
+        if back_nn_list is not None:
+            if back_nn_index < len(back_nn_list) - 1:
+                back_nn_list[back_nn_index + 1].back_propagate(
+                    propagated_list=back_propagated_list,
+                    learning_rate=learning_rate,
+                    momentum_factor=momentum_factor,
+                    back_nn_list=back_nn_list,
+                    back_nn_index=back_nn_index + 1
+                )

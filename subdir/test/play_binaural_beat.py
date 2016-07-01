30eb3347d3a3a3b943ebc33e1f2ff8f062d962a0
Chimera <kimera0kimaira@gmail.com>
2016-07-01 19:41:11 +0900

    Create play_binaural_beat.py

---
+++ subdir/test/play_binaural_beat.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+from brainbeat.binaural_beat import BinauralBeat
+from waveform.sine_wave import SineWave
+
+
+def main():
+    wave_form = SineWave()
+    brain_beat = BinauralBeat()
+    brain_beat.wave_form = wave_form
+    brain_beat.play_beat(frequencys=(300, 310), play_time=10, volume=0.5)
+
+if __name__ == "__main__":
+    main()

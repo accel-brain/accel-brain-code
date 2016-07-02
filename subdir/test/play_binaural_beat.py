23915971a316225b76f98dc9a5b56c43253f1f0d
Chimera <kimera0kimaira@gmail.com>
2016-07-02 17:46:51 +0900

    Update play_binaural_beat.py

--- subdir/test/play_binaural_beat.py
+++ subdir/test/play_binaural_beat.py
@@ -5,10 +5,8 @@ from AccelBrainBeat.waveform.sine_wave import SineWave
 
 
 def main():
-    wave_form = SineWave()
     brain_beat = BinauralBeat()
-    brain_beat.wave_form = wave_form
-    brain_beat.play_beat(frequencys=(300, 310), play_time=10, volume=0.5)
+    brain_beat.play_beat(frequencys=(300, 310), play_time=10, volume=0.01)
 
 if __name__ == "__main__":
     main()

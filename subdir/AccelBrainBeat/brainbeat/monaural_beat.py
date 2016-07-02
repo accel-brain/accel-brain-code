b80178de59122ec4754daf11a607a4da6a587f67
Chimera <kimera0kimaira@gmail.com>
2016-07-02 21:09:12 +0900

    Update monaural_beat.py

--- subdir/AccelBrainBeat/brainbeat/monaural_beat.py
+++ subdir/AccelBrainBeat/brainbeat/monaural_beat.py
@@ -33,3 +33,28 @@ class MonauralBeat(BrainBeat):
             chunk = (left_chunk[i] + right_chunk[i]) * volume
             data = struct.pack("2f", chunk, chunk)
             stream.write(data)
+
+    def read_stream(self, left_chunk, right_chunk, volume, bit16=32767.0):
+        '''
+        具象メソッド
+        wavファイルに保存するモノラルビートを読み込む
+
+        Args:
+            left_chunk:     左音源に対応するチャンク
+            right_chunk:    右音源に対応するチャンク
+            volume:         音量
+            bit16:          整数化の条件
+
+        Returns:
+            フレームのlist
+        '''
+        if len(left_chunk) != len(right_chunk):
+            raise ValueError()
+
+        frame_list = []
+        for i in range(len(left_chunk)):
+            chunk = int((left_chunk[i] + right_chunk[i]) * bit16 * volume)
+            data = struct.pack("2h", chunk, chunk)
+            frame_list.append(data)
+
+        return frame_list

905526c4044178b465f6db0778f2cd873f90a9eb
Chimera <kimera0kimaira@gmail.com>
2016-10-03 00:34:25 +0900

    Update that has been forgotten...

--- deep-learning-by-means-of-design-pattern/synapse_list.py
+++ deep-learning-by-means-of-design-pattern/synapse_list.py
@@ -1,6 +1,7 @@
 #!/user/bin/env python
 # -*- coding: utf-8 -*-
 import random
+from accelstat.normalization import Normalization
 
 
 class Synapse(object):
@@ -54,6 +55,9 @@ class Synapse(object):
     weights_dict = property(get_weights_dict, set_weights_dict)
     diff_weights_dict = property(get_diff_weights_dict, set_diff_weights_dict)
 
+    # 規格化オブジェクト
+    __normalization = None
+
     def create_node(self, shallower_neuron_list, deeper_neuron_list, weights_dict=None):
         '''
         グラフにノードのリンクをセットする
@@ -79,3 +83,37 @@ class Synapse(object):
         for i, j in self.diff_weights_dict:
             self.weights_dict[(i, j)] += self.diff_weights_dict[(i, j)]
             self.diff_weights_dict[(i, j)] = 0.0
+
+    def normalize_bias_and_weights(self):
+        '''
+        規格化する
+        '''
+        if self.__normalization is None:
+            self.__normalization = Normalization()
+
+        visible_activity_list = [self.shallower_neuron_list[i].activity for i in range(len(self.shallower_neuron_list))]
+        hidden_activity_list = [self.deeper_neuron_list[i].activity for i in range(len(self.deeper_neuron_list))]
+
+        visible_activity_list = self.__normalization.z_theta(visible_activity_list)
+        hidden_activity_list = self.__normalization.z_theta(hidden_activity_list)
+
+        for i in range(len(visible_activity_list)):
+            self.shallower_neuron_list[i].activity = visible_activity_list[i]
+
+        for i in range(len(hidden_activity_list)):
+            self.deeper_neuron_list[i].activity = hidden_activity_list[i]
+
+        weights_list = []
+        for i in range(len(self.shallower_neuron_list)):
+            for j in range(len(self.deeper_neuron_list)):
+                weights_list.append(self.weights_dict[(i, j)])
+
+        for i in range(len(self.shallower_neuron_list)):
+            for j in range(len(self.deeper_neuron_list)):
+                self.weights_dict[(i, j)] = self.__normalization.once(
+                    x=self.weights_dict[(i, j)],
+                    x_min=min(weights_list),
+                    x_max=max(weights_list),
+                    range_min=0.0,
+                    range_max=1.0
+            )

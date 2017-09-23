03f0e09efab3e7c8eac590ed7602adececc22673
chimera0 <ai-brain-lab@accel-brain.com>
2017-09-23 18:41:49 +0900

    Create setuptool.

---
+++ Automatic-Summarization/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='pysummarization',
+    version='1.0.1',
+    description='pysummarization is Python library for the automatic summarization, document abstraction, and text filtering.',
+    long_description='The function of this library is automatic summarization using a kind of natural language processing. This library enable you to create a summary with the major points of the original document or web-scraped text that filtered by text clustering.',
+    url='https://github.com/chimera0/accel-brain-code/tree/master/Automatic-Summarization',
+    author='chimera0',
+    author_email='ai-brain-lab@accel-brain.com',
+    license='GPL2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Topic :: Text Processing',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='Automatic summarization document abstraction abstract text filtering',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['numpy', 'nltk', 'mecab-python3'],
+)

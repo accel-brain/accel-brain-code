d1ab29cb532a8a249cf93ec0e9787789e5ffbe6f
RUM <kimera0kimaira@gmail.com>
2017-10-01 18:11:15 +0900

    Update feature_point_neuron.pyx

--- Deep-Learning-by-means-of-Design-Pattern/pydbm/neuron/feature_point_neuron.pyx
+++ Deep-Learning-by-means-of-Design-Pattern/pydbm/neuron/feature_point_neuron.pyx
@@ -1,4 +1,3 @@
-#!/user/bin/env python
 # -*- coding: utf-8 -*-
 import pyximport;pyximport.install()
 import random
@@ -10,14 +9,13 @@ from pydbm.activation.interface.activating_function_interface import ActivatingF
 
 class FeaturePointNeuron(Neuron, VisibleLayerInterface, HiddenLayerInterface):
     '''
-    深層ボルツマンマシンの特徴点を疑似的な観測データ点と見立てることを前提に、
-    隠れ層のニューロンを疑似的な可視層ニューロンとしてインスタンス化するクラス
+    For considering the feature points as `virtual` observed data points,
+    instantiate neurons in hidden layer as the neurons in visible layer.
 
-    隠れ層のニューロンの機能的等価物であると共に、
-    可視層のニューロンの機能的等価物としても実装する
+    This object is functionally equivalent to neurons in hidden layer and visible layer.
     '''
 
-    # 可視層のニューロン
+    # The object of neurons in visible layer.
     __visible_layer_interface = None
 
     def get_activating_function(self):
@@ -36,13 +34,12 @@ class FeaturePointNeuron(Neuron, VisibleLayerInterface, HiddenLayerInterface):
 
     def __init__(self, visible_layer_interface):
         '''
-        初期化
-        自分自身をvisible_layer_interfaceの型で仕様化すると共に、
-        本来のvisible_layer_interfaceを実現した可視層ニューロンを包含することで、
-        自分自身も可視層ニューロンとして振る舞えるようにする
+        Initialize `self` as not only the instance of `visible_layer_interface` 
+        but also inheritance of `visible_layer_interface`
+        so that `self` can activate as neurons in visible layer.
 
         Args:
-            visible_layer_interface:    隠れ層のニューロンのインターフェイスを実装したニューロンオブジェクト
+            visible_layer_interface:    The object of neurons.
         '''
         self.bias = round(random.random(), 3)
         visible_layer_interface.bias = self.bias
@@ -50,22 +47,20 @@ class FeaturePointNeuron(Neuron, VisibleLayerInterface, HiddenLayerInterface):
 
     def observe_data_point(self, double x):
         '''
-        インターフェイス実現
-        観測データ点の入力
-
+        Input observed data points.
+        
         Args:
-            x:  観測データ点
+            x:  observed data points.
         '''
         self.__visible_layer_interface.observe_data_point(x)
         self.activity = x
 
     def visible_update_state(self, double link_value):
         '''
-        インターフェイス実現
-        可視層ニューロンとしての学習
+        Update activity as neurons in visible layer.
 
         Args:
-            link_value:      リンク先による入力値
+            link_value:      Input value.
 
         '''
         self.__visible_layer_interface.visible_update_state(link_value)
@@ -73,22 +68,20 @@ class FeaturePointNeuron(Neuron, VisibleLayerInterface, HiddenLayerInterface):
 
     def hidden_update_state(self, double link_value):
         '''
-        インターフェイス実現
-        隠れ層ニューロンとしての学習
+        Update activity as neurons in hidden layer.
 
         Args:
-            link_value:      リンク先による入力値
+            link_value:      Input value.
 
         '''
         self.visible_update_state(link_value)
 
     def update_bias(self, double learning_rate):
         '''
-        具象メソッド
-        バイアスの調整
+        Update biases.
 
         Args:
-            learning_rate:  学習率
+            learning_rate:  Learning rate.
         '''
         diff_bias = learning_rate * self.activity
         self.__visible_layer_interface.diff_bias += diff_bias

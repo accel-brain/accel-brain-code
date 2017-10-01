3538738d6cad17c4b3176413a004395963aa58c4
RUM <kimera0kimaira@gmail.com>
2017-10-01 18:26:09 +0900

    Update complete_bipartite_graph.pyx

--- Deep-Learning-by-means-of-Design-Pattern/pydbm/synapse/complete_bipartite_graph.pyx
+++ Deep-Learning-by-means-of-Design-Pattern/pydbm/synapse/complete_bipartite_graph.pyx
@@ -7,43 +7,37 @@ from pydbm.synapse_list import Synapse
 
 class CompleteBipartiteGraph(Synapse):
     '''
-    シナプスを有したニューラル・ネットワークとしての完全二部グラフ
+    Complete Bipartite Graph.
+    
+    The shallower layer is to the deeper layer what the visible layer is to the hidden layer.
     '''
 
     def get_visible_neuron_list(self):
-        '''
-        比較的浅い層を可視層と見立てる
-        '''
+        ''' getter '''
         return self.shallower_neuron_list
 
     def set_visible_neuron_list(self, value):
-        '''
-        比較的浅い層を可視層と見立てる
-        '''
+        ''' setter '''
         self.shallower_neuron_list = value
 
     visible_neuron_list = property(get_visible_neuron_list, set_visible_neuron_list)
 
     def get_hidden_neuron_list(self):
-        '''
-        比較的深い層を隠れ層と見立てる
-        '''
+        ''' getter '''
         return self.deeper_neuron_list
 
     def set_hidden_neuron_list(self, value):
-        '''
-        比較的深い層を隠れ層と見立てる
-        '''
+        ''' setter '''
         self.deeper_neuron_list = value
 
     hidden_neuron_list = property(get_hidden_neuron_list, set_hidden_neuron_list)
     
     def update(self, double learning_rate):
         '''
-        各リンクの重みを更新する
+        Update weights.
 
         Args:
-            learning_rate:  学習率
+            learning_rate:  Learning rate.
         '''
         cdef int i
         visible_activity_arr = np.array([[self.visible_neuron_list[i].activity] * len(self.hidden_neuron_list) for i in range(len(self.visible_neuron_list))])

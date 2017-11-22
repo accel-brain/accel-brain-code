40791a8eecea233bec376fa821fa64cbb5da7a3a
chimera0 <ai-brain-lab@accel-brain.com>
2017-11-22 10:27:06 +0900

    Update for test.

--- Reinforcement-Learning/demo_maze_deep_boltzmann_q_learning.py
+++ Reinforcement-Learning/demo_maze_deep_boltzmann_q_learning.py
@@ -10,20 +10,41 @@ from pydbm.activation.logistic_function import LogisticFunction
 if __name__ == "__main__":
     # "S": Start point, "G": End point(goal), "#": wall, "#": Agent.
     start_point_label, end_point_label, wall_label, agent_label = ("S", "G", "#", "@")
-    map_d = 20
-    map_arr = 10 * np.random.rand(map_d, map_d)
-    map_arr = map_arr.astype(int)
-    map_arr += np.diag(list(range(map_d))) * 10
+    map_d = 100
+    map_arr = np.random.rand(map_d, map_d)
+    map_arr += np.diag(list(range(map_d)))
 
+    vector_list_list = []
+    for y in range(map_arr.shape[0]):
+        for x in range(map_arr.shape[1]):
+            vector_list = []
+            vector_list.append(map_arr[y][x])
+            for _x in [-1, 1]:
+                __x = x + _x
+                for _y in [-1, 1]:
+                    __y = y + _y
+                    if __x < 0 or __y < 0:
+                        vector = 0
+                    else:
+                        try:
+                            vector = map_arr[__y][__x]
+                        except IndexError:
+                            vector = 0
+                    vector_list.append(vector)
+            vector_list_list.append(vector_list)
+    vector_arr = np.array(vector_list_list)
+    vector_arr = vector_arr.astype(float)
     dbm = StackedAutoEncoder(
         DBMMultiLayerBuilder(),
-        [map_arr.shape[1], map_arr.shape[1], map_arr.shape[1]],
+        [vector_arr.shape[1], vector_arr.shape[1], 10],
         LogisticFunction(),
         ContrastiveDivergence(),
-        0.05
+        0.005
     )
-    dbm.learn(map_arr.astype(float), traning_count=1)
-    feature_map_arr = dbm.feature_points_arr
+    dbm.learn(vector_arr, traning_count=1)
+    feature_arr = dbm.feature_points_arr
+    feature_arr = feature_arr[:, 0]
+    feature_map_arr = feature_arr.reshape(map_d, map_d)
 
     map_arr = map_arr.astype(object)
     map_arr[:, 0] = wall_label
@@ -53,7 +74,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 3:
         gamma_value = float(sys.argv[3])
 
-    greedy_rate = 0.75
+    greedy_rate = 0.25
     if len(sys.argv) > 4:
         greedy_rate = float(sys.argv[4])
 

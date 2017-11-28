83dce171e74ccc608fee7e893c7bf55755db807e
chimera0 <ai-brain-lab@accel-brain.com>
2017-11-28 20:31:07 +0900

    Create.

---
+++ Reinforcement-Learning-with-js/qlearning/boltzmannqlearning.js
@@ -0,0 +1,19 @@
+/*
+Repository name: accel-brain-code
+Description: base class and `Template Method Pattern` of Q-Learning.
+Version: 1.0.1
+Author: chimera0(RUM)
+Author URI: http://accel-brain.com/
+License: GNU General Public License v2.0
+Copyright 2017 chimera0 (email : ai-brain-lab@accel-brain.com)
+*/
+
+
+/**
+ * Q-Learning with Boltzmann distribution.
+ *
+ */
+var BoltzmannQLearning = (function()
+{
+
+}) ();

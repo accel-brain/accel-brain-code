9fea5ea1a0eed8b92105cf2bda64932c9f96505e
chimera0 <ai-brain-lab@accel-brain.com>
2018-02-13 14:11:34 +0900

    Update for dev.

--- Deep-Learning-by-means-of-Design-Pattern/mxnet/demo_stacked_auto_encoder.py
+++ Deep-Learning-by-means-of-Design-Pattern/mxnet/demo_stacked_auto_encoder.py
@@ -12,15 +12,32 @@ from pydbm_mxnet.activation.relu_function import ReLuFunction
 if __name__ == "__main__":
     '''
     '''
-    target_arr = mx.ndarray.random_poisson(shape=(10000, 1000), dtype="float32")
+    target_arr = mx.ndarray.random_poisson(shape=(1000, 100), dtype="float32")
 
     dbm = StackedAutoEncoder(
         DBMMultiLayerBuilder(),
         [target_arr.shape[1], 10, target_arr.shape[1]],
-        [ReLuFunction(), LogisticFunction(), ReLuFunction()],
+        [LogisticFunction(), LogisticFunction(), LogisticFunction()],
         ContrastiveDivergence(),
         0.0005,
         0.25
     )
     dbm.learn(target_arr, traning_count=1)
+    print("-" * 100)
+    print("Feature points: ")
     print(dbm.feature_points_arr)
+    print("-" * 100)
+    print("Visible activity: ")
+    print(dbm.get_visible_activity_arr_list()[0])
+    print("-" * 100)
+    print("Hidden activity: ")
+    print(dbm.get_hidden_activity_arr_list()[0])
+    print("-" * 100)
+    print("Visible bias: ")
+    print(dbm.get_visible_bias_arr_list()[0])
+    print("-" * 100)
+    print("Hidden bias: ")
+    print(dbm.get_hidden_bias_arr_list()[0])
+    print("-" * 100)
+    print("Weights: ")
+    print(dbm.get_weight_arr_list()[0])

1d96aff4704af6e15bc549b13ee315becf4a0421
RUM <ai-brain-lab@accel-brain.com>
2018-05-30 22:23:25 +0900

    Update setup.py

--- Reinforcement-Learning/setup.py
+++ Reinforcement-Learning/setup.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-def read_rst(file_name):
+def read_readme(file_name):
     from os import path
-    with open(path.join(path.dirname(__file__), file_name)) as f:
-        rst = f.read()
-    return rst
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+    return long_description
 
 
 setup(
     name='pyqlearning',
-    version='1.0.9',
+    version='1.1.0',
     description='pyqlearning is Python library to implement Reinforcement Learning, especially for Q-Learning.',
-    long_description=read_rst("README.rst"),
+    long_description=read_readme("README.md"),
+    long_description_content_type='text/markdown'
     url='https://github.com/chimera0/accel-brain-code/tree/master/Reinforcement-Learning',
     author='chimera0',
     author_email='ai-brain-lab@accel-brain.com',

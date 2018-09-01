6dedf157f7b889f4e264a6896f85b751bdf0611a
RUM <chimera@chimera-network.c.neural-truth-88311.internal>
2018-09-01 23:15:42 +0900

    Update version.

--- Algorithmic-Composition/setup.py
+++ Algorithmic-Composition/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycomposer',
-    version='0.0.2',
+    version='0.0.3',
     description='pycomposer is Python library for Algorithmic Composition or Automatic Composition based on the stochastic music theory. Especialy, this library provides apprication of the generative model such as a Restricted Boltzmann Machine(RBM). And the Monte Carlo method such as Quantum Annealing model is used in this library as optimizer of compositions.',
     long_description='pycomposer is Python library for Algorithmic Composition or Automatic Composition by Reinforcement Learning such as Q-Learning and Recurrent Temporal Restricted Boltzmann Machine(RTRBM). Q-Learning and RTRBM in this library allows you to extract the melody information about a MIDI tracks and these models can learn and inference patterns of the melody. And This library has wrapper class for converting melody data inferenced by Q-Learning and RTRBM into MIDI file.',
     url='https://github.com/chimera0/accel-brain-code/tree/master/Algorithmic-Composition',

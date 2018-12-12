3823a3a3c571d6f84ef08563f3f3e834dc173ffc
chimera0 <ai-brain-lab@accel-brain.com>
2018-12-12 20:50:04 +0900

    Add ConvLSTM.

--- Reinforcement-Learning/pyqlearning/functionapproximator/convolutional_lstm_fc_fa.py
+++ Reinforcement-Learning/pyqlearning/functionapproximator/convolutional_lstm_fc_fa.py
@@ -12,7 +12,7 @@ from pydbm.optimization.opt_params import OptParams
 from pydbm.verification.interface.verificatable_result import VerificatableResult
 from pydbm.loss.interface.computable_loss import ComputableLoss
 
-from pydbm.rnn.lstmmodel.conv_lstm_model import ConvLSTMModel
+from pydbm.rnn.lstm_model import LSTMModel
 
 # Loss function.
 from pydbm.loss.mean_squared_error import MeanSquaredError
@@ -22,23 +22,27 @@ from pydbm.optimization.optparams.adam import Adam
 from pydbm.verification.verificate_function_approximation import VerificateFunctionApproximation
 
 
-class ConvolutionalLSTMFA(FunctionApproximator):
+class ConvolutionalLSTMFCFA(FunctionApproximator):
     '''
-    Convolutional LSTM Networks as a Function Approximator,
-    which is a model that structurally couples convolution operators to LSTM networks, 
-    can be utilized as components in constructing the Function Approximator.
+    Convolutional LSTM Networks as a Function Approximator like CLDNN Architecture(Sainath, T. N, et al., 2015).
+    
+    This is a model of the function approximator which loosely coupled CNN and LSTM.
+    Like CLDNN Architecture(Sainath, T. N, et al., 2015), this model uses CNNs to reduce 
+    the spectral variation of the input feature of rewards, and then passes this to LSTM 
+    layers to perform temporal modeling, and finally outputs this to DNN layers, 
+    which produces a feature representation of Q-Values that is more easily separable.
     
     References:
         - Sainath, T. N., Vinyals, O., Senior, A., & Sak, H. (2015, April). Convolutional, long short-term memory, fully connected deep neural networks. In Acoustics, Speech and Signal Processing (ICASSP), 2015 IEEE International Conference on (pp. 4580-4584). IEEE.
-        - Xingjian, S. H. I., Chen, Z., Wang, H., Yeung, D. Y., Wong, W. K., & Woo, W. C. (2015). Convolutional LSTM network: A machine learning approach for precipitation nowcasting. In Advances in neural information processing systems (pp. 802-810).
     '''
     
-    __next_action_arr_list = []
+    __q_arr_list = []
     
     def __init__(
         self,
         batch_size,
-        conv_lstm_model,
+        layerable_cnn_list,
+        lstm_model,
         seq_len=10,
         learning_rate=1e-05,
         computable_loss=None,
@@ -67,9 +71,9 @@ class ConvolutionalLSTMFA(FunctionApproximator):
             
         self.__logger.addHandler(handler)
 
-        if isinstance(conv_lstm_model, ConvLSTMModel) is False:
+        if isinstance(lstm_model, LSTMModel) is False:
             raise TypeError()
-
+        
         if computable_loss is None:
             computable_loss = MeanSquaredError()
         if verificatable_result is None:
@@ -77,12 +81,33 @@ class ConvolutionalLSTMFA(FunctionApproximator):
         if opt_params is None:
             opt_params = Adam()
 
-        self.__conv_lstm_model = conv_lstm_model
+        cnn = ConvolutionalNeuralNetwork(
+            # The `list` of `ConvolutionLayer`.
+            layerable_cnn_list=layerable_cnn_list,
+            # The number of epochs in mini-batch training.
+            epochs=200,
+            # The batch size.
+            batch_size=batch_size,
+            # Learning rate.
+            learning_rate=learning_rate,
+            # Loss function.
+            computable_loss=computable_loss,
+            # Optimizer.
+            opt_params=opt_params,
+            # Verification.
+            verificatable_result=verificatable_result,
+            # Others.
+            learning_attenuate_rate=0.1,
+            attenuate_epoch=50
+        )
+        self.__cnn = cnn
+        self.__lstm_model = lstm_model
         self.__seq_len = seq_len
         self.__batch_size = batch_size
         self.__computable_loss = computable_loss
         self.__learning_rate = learning_rate
         self.__verbose_mode = verbose_mode
+        self.__q_shape = None
         self.__q_logs_list = []
 
     def learn_q(self, q, new_q):
@@ -93,14 +118,31 @@ class ConvolutionalLSTMFA(FunctionApproximator):
             q:                  Predicted Q-Value.
             new_q:              Real Q-Value.
         '''
+        if self.__q_shape is None:
+            raise ValueError("Before learning, You should execute `__inference_q`.")
+
         q_arr = np.array([q] * self.__batch_size).reshape(-1, 1)
         new_q_arr = np.array([new_q] * self.__batch_size).reshape(-1, 1)
         cost_arr = self.__computable_loss.compute_loss(q_arr, new_q_arr)
         delta_arr = self.__computable_loss.compute_delta(q_arr, new_q_arr)
         delta_arr = delta_arr / self.__batch_size
-        delta_arr, grads_list = self.__conv_lstm_model.back_propagation(q_arr, delta_arr)
+        delta_arr, lstm_output_grads_list = self.__lstm_model.output_back_propagate(
+            q_arr,
+            delta_arr
+        )
 
-        self.__conv_lstm_model.optimize(grads_list, self.__learning_rate, 1)
+        delta_arr, lstm_hidden_grads_list = self.__lstm_model.hidden_back_propagate(
+            delta_arr
+        )
+
+        lstm_grads_list = lstm_output_grads_list
+        lstm_grads_list.extend(lstm_hidden_grads_list)
+
+        delta_arr = delta_arr[:, -1].reshape(self.__q_shape)
+        delta_arr = self.__cnn.back_propagation(delta_arr)
+
+        self.__lstm_model.optimize(lstm_grads_list, self.__learning_rate, 1)
+        self.__cnn.optimize(self.__learning_rate, 1)
         self.__q_logs_list.append((q, new_q, cost_arr.mean()))
 
     def inference_q(self, next_action_arr):
@@ -113,16 +155,18 @@ class ConvolutionalLSTMFA(FunctionApproximator):
         Returns:
             `np.ndarray` of Q-Values.
         '''
-        self.__next_action_arr_list.append(next_action_arr)
-        while len(self.__next_action_arr_list) > self.__seq_len:
-            self.__next_action_arr_list = self.__next_action_arr_list[1:]
-        while len(self.__next_action_arr_list) < self.__seq_len:
-            self.__next_action_arr_list.append(self.__next_action_arr_list[-1])
-
-        _next_action_arr = np.array(self.__next_action_arr_list)
-        _next_action_arr = _next_action_arr.transpose((1, 0, 2, 3, 4))
-
-        q_arr = self.__conv_lstm_model.inference(_next_action_arr)
+        q_arr = self.__cnn.inference(next_action_arr)
+        self.__q_shape = q_arr.shape
+        q_arr = q_arr.reshape((q_arr.shape[0], -1))
+        
+        self.__q_arr_list.append(q_arr)
+        while len(self.__q_arr_list) > self.__seq_len:
+            self.__q_arr_list = self.__q_arr_list[1:]
+        while len(self.__q_arr_list) < self.__seq_len:
+            self.__q_arr_list.append(self.__q_arr_list[-1])
+        q_arr = np.array(self.__q_arr_list)
+        q_arr = q_arr.transpose((1, 0, 2))
+        q_arr = self.__lstm_model.inference(q_arr)
         return q_arr[:, -1]
 
     def get_q_logs_list(self):

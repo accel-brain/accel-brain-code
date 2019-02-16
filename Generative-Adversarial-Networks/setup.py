1f0fc9b9c76f9bc08c3704bc55f259124eb60763
chimera0 <ai-brain-lab@accel-brain.com>
2019-02-17 00:43:38 +0900

    Update for v0.0.1.

---
+++ Generative-Adversarial-Networks/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+def read_readme(file_name):
+    from os import path
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, file_name), encoding='utf-8') as f:
+        long_description = f.read()
+
+    return long_description
+
+
+setup(
+    name='pygan',
+    version='0.0.1',
+    description='pygan is Python library to implement Generative Adversarial Networks.',
+    long_description=read_readme("README.md"),
+    long_description_content_type='text/markdown',
+    url='https://github.com/chimera0/accel-brain-code/tree/master/Generative-Adversarial-Networks/',
+    author='chimera0',
+    author_email='ai-brain-lab@accel-brain.com',
+    license='GPL2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Framework :: Robot Framework',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='Q-Learning Deep Q-Network DQN DBM Reinforcement Learning Boltzmann Multi-agent LSTM CNN Convolution',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['numpy', 'pandas'],
+)

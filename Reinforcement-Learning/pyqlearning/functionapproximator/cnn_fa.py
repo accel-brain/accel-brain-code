34f0b2bae24410081bce1904acc3c108d7895ef1
chimera0 <ai-brain-lab@accel-brain.com>
2019-02-16 15:19:30 +0900

    Update CNN's output layer.

--- Reinforcement-Learning/pyqlearning/functionapproximator/cnn_fa.py
+++ Reinforcement-Learning/pyqlearning/functionapproximator/cnn_fa.py
@@ -11,6 +11,7 @@ from pydbm.cnn.feature_generator import FeatureGenerator
 from pydbm.optimization.opt_params import OptParams
 from pydbm.verification.interface.verificatable_result import VerificatableResult
 from pydbm.loss.interface.computable_loss import ComputableLoss
+from pydbm.synapse.cnn_output_graph import CNNOutputGraph
 
 # Loss function.
 from pydbm.loss.mean_squared_error import MeanSquaredError
@@ -44,13 +45,13 @@ class CNNFA(FunctionApproximator):
         self,
         batch_size,
         layerable_cnn_list,
+        cnn_output_graph,
         learning_rate=1e-05,
         computable_loss=None,
         opt_params=None,
         verificatable_result=None,
         pre_learned_path_list=None,
-        fc_w_arr=None,
-        fc_activation_function=None,
+        pre_learned_output_path=None,
         cnn=None,
         verbose_mode=False
     ):
@@ -60,6 +61,7 @@ class CNNFA(FunctionApproximator):
         Args:
             batch_size:                     Batch size in mini-batch.
             layerable_cnn_list:             `list` of `LayerableCNN`.
+            cnn_output_graph:               Computation graph which is-a `CNNOutputGraph` to compute parameters in output layer.
             learning_rate:                  Learning rate.
             computable_loss:                is-a `ComputableLoss`.
             opt_params:                     is-a `OptParams`.
@@ -67,8 +69,8 @@ class CNNFA(FunctionApproximator):
             pre_learned_path_list:          `list` of file path that stored pre-learned parameters.
                                             This parameters will be refered only when `cnn` is `None`.
 
-            fc_w_arr:                       `np.ndarray` of weight matrix in output layer.
-            fc_activation_function:         is-a `ActivatingFunctionInterface`.
+            pre_learned_output_path:        File path that stores pre-learned parameters.
+
             cnn:                            is-a `ConvolutionalNeuralNetwork` as a model in this class.
                                             If not `None`, `self.__cnn` will be overrided by this `cnn`.
                                             If `None`, this class initialize `ConvolutionalNeuralNetwork`
@@ -128,15 +130,13 @@ class CNNFA(FunctionApproximator):
                 learning_attenuate_rate=0.1,
                 attenuate_epoch=50
             )
+            cnn.setup_output_layer(cnn_output_graph, pre_learned_output_path)
 
         self.__cnn = cnn
         self.__batch_size = batch_size
         self.__computable_loss = computable_loss
         self.__learning_rate = learning_rate
         self.__verbose_mode = verbose_mode
-        self.__fc_w_arr = fc_w_arr
-        self.__fc_activation_function = fc_activation_function
-        self.__q_shape = None
         self.__loss_list = []
 
     def learn_q(self, predicted_q_arr, real_q_arr):
@@ -147,15 +147,13 @@ class CNNFA(FunctionApproximator):
             predicted_q_arr:    `np.ndarray` of predicted Q-Values.
             real_q_arr:         `np.ndarray` of real Q-Values.
         '''
+        """
         if self.__q_shape is None:
             raise ValueError("Before learning, You should execute `__inference_q`.")
+        """
 
         loss = self.__computable_loss.compute_loss(predicted_q_arr, real_q_arr)
         delta_arr = self.__computable_loss.compute_delta(predicted_q_arr, real_q_arr)
-        # This is a constant parameter and will be not updated.
-        delta_arr = np.dot(delta_arr, self.__fc_w_arr.T)
-
-        delta_arr = delta_arr.reshape(self.__q_shape)
         delta_arr = self.__cnn.back_propagation(delta_arr)
         self.__cnn.optimize(self.__learning_rate, 1)
         self.__loss_list.append(loss)
@@ -171,12 +169,6 @@ class CNNFA(FunctionApproximator):
             `np.ndarray` of Q-Values.
         '''
         q_arr = self.__cnn.inference(next_action_arr)
-        self.__q_shape = q_arr.shape
-        q_arr = q_arr.reshape((q_arr.shape[0], -1))
-        if self.__fc_w_arr is None:
-            self.__fc_w_arr = np.random.normal(size=(q_arr.shape[-1], 1)) * 0.01
-        q_arr = np.dot(q_arr, self.__fc_w_arr)
-        q_arr = self.__fc_activation_function.activate(q_arr)
         return q_arr
 
     def get_model(self):

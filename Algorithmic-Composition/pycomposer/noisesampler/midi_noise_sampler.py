6e67500a2fd877c0e00bf57ec9f15c2dbd5e26d2
chimera0 <ai-brain-lab@accel-brain.com>
2019-03-26 22:18:43 +0900

    Update for pygan.

---
+++ Algorithmic-Composition/pycomposer/noisesampler/midi_noise_sampler.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from pygan.noise_sampler import NoiseSampler
+
+
+class MidiNoiseSampler(NoiseSampler):
+    '''
+    Generate samples based on the noise prior by Gauss distribution.
+    '''
+
+    def __init__(
+        self, 
+        batch_size=20,
+        pitch_binomial_prob=0.5
+    ):
+        '''
+        Init.
+
+        Args:
+            velocity_low:           Lower boundary of the output interval of Uniform noise for velocity.
+            velocity_high:          Higher boundary of the output interval of Uniform noise for velocity.
+        '''
+        self.__batch_size = batch_size
+        self.__pitch_binomial_prob = pitch_binomial_prob
+
+    def generate(self):
+        '''
+        Generate noise samples.
+        
+        Returns:
+            `np.ndarray` of samples.
+        '''
+        generated_arr = np.random.uniform(
+            low=-1,
+            high=1,
+            size=((self.__batch_size, 1, 12))
+        )
+
+        if self.noise_sampler is not None:
+            generated_arr += self.noise_sampler.generate()
+
+        return generated_arr
---
+++ Algorithmic-Composition/pycomposer/truesampler/midi_true_sampler.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from pygan.true_sampler import TrueSampler
+from pycomposer.midi_vectorlizer import MidiVectorlizer
+
+
+class MidiTrueSampler(TrueSampler):
+    '''
+    Sampler which draws samples from the `true` distribution of MIDI files.
+    '''
+
+    def __init__(
+        self, 
+        midi_path_list, 
+        batch_size=20, 
+        seq_len=10, 
+        time_fraction=0.1
+    ):
+        '''
+        Init.
+
+        Args:
+            midi_path_list:     `list` of paths to MIDI files.
+            batch_size:         Batch size.
+            seq_len:            The length of sequneces.
+                                The length corresponds to the number of `time` splited by `time_fraction`.
+
+            time_fraction:      Time fraction.
+        '''
+        midi_vectorlizer = MidiVectorlizer()
+        self.__midi_df_list = [None] * len(midi_path_list)
+        for i in range(len(midi_path_list)):
+            self.__midi_df_list[i] = midi_vectorlizer.extract(midi_path_list[i])
+
+        self.__batch_size = batch_size
+        self.__seq_len = seq_len
+        self.__time_fraction = time_fraction
+
+    def draw(self):
+        '''
+        Draws samples from the `true` distribution.
+        
+        Returns:
+            `np.ndarray` of samples.
+        '''
+        sampled_arr = np.empty((self.__batch_size, self.__seq_len, 12))
+
+        for batch in range(self.__batch_size):
+            key = np.random.randint(low=0, high=len(self.__midi_df_list))
+            midi_df = self.__midi_df_list[key]
+            program_arr = midi_df.program.drop_duplicates().values
+            key = np.random.randint(low=0, high=program_arr.shape[0])
+            program_key = program_arr[key]
+            midi_df = midi_df[midi_df.program == program_key]
+            if midi_df.shape[0] < self.__seq_len:
+                raise ValueError("The length of musical performance (program: " + str(program_key) + " is short.")
+
+            row = np.random.uniform(
+                low=midi_df.start.min(), 
+                high=midi_df.end.max() - (self.__seq_len * self.__time_fraction)
+            )
+            for seq in range(self.__seq_len):
+                df = midi_df[midi_df.start > row + (seq * self.__time_fraction)]
+                df = df[df.end < row + ((seq+1) * self.__time_fraction)]
+                sampled_arr[batch, seq] = self.__convert_into_feature(df)
+
+        if sampled_arr.max() > sampled_arr.min():
+            sampled_arr = (sampled_arr - sampled_arr.min()) / (sampled_arr.max() - sampled_arr.min())
+        return sampled_arr
+
+    def __convert_into_feature(self, df):
+        arr = np.zeros(12)
+        for i in range(df.shape[0]):
+            arr[df.pitch.values[i] % 12] = df.velocity.values[i]
+
+        return arr.reshape(1, -1).astype(float)

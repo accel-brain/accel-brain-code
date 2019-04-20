688af0abbed3b286d70ec37a33b2ac25a86c9b22
chimera0 <ai-brain-lab@accel-brain.com>
2019-04-20 23:29:02 +0900

    Update for version.

--- Algorithmic-Composition/setup.py
+++ Algorithmic-Composition/setup.py
@@ -12,8 +12,8 @@ def read_readme(file_name):
 
 setup(
     name='pycomposer',
-    version='1.0.1',
-    description='pycomposer is Python library for Algorithmic Composition or Automatic Composition based on the stochastic music theory and the Statistical machine learning problems. Especialy, this library provides apprication of the Algorithmic Composer based on Generative Adversarial Networks(GANs).',
+    version='1.0.2',
+    description='pycomposer is Python library for Algorithmic Composition or Automatic Composition based on the stochastic music theory and the Statistical machine learning problems. Especialy, this library provides apprication of the Algorithmic Composer based on Generative Adversarial Networks(GANs) and Conditional Generative Adversarial Networks(Conditional GANs).',
     long_description=read_readme("README.md"),
     long_description_content_type='text/markdown',
     url='https://github.com/chimera0/accel-brain-code/tree/master/Algorithmic-Composition',
@@ -29,7 +29,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 3',
     ],
-    keywords='GAN GANs MIDI Composition Generative Adversarial Networks',
+    keywords='GAN GANs MIDI Composition Generative Adversarial Networks Conditional MidiNet MuseGAN',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['numpy', 'pandas', 'pretty_midi', 'pygan', 'pydbm'],
 )

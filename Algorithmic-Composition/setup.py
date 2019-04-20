20a56b9045eb47cd15ee85d861b002626a1af0fb
RUM <ai-brain-lab@accel-brain.com>
2019-04-20 23:36:35 +0900

    Update setup.py

--- Algorithmic-Composition/setup.py
+++ Algorithmic-Composition/setup.py
@@ -12,7 +12,7 @@ def read_readme(file_name):
 
 setup(
     name='pycomposer',
-    version='1.0.2',
+    version='1.0.1',
     description='pycomposer is Python library for Algorithmic Composition or Automatic Composition based on the stochastic music theory and the Statistical machine learning problems. Especialy, this library provides apprication of the Algorithmic Composer based on Generative Adversarial Networks(GANs) and Conditional Generative Adversarial Networks(Conditional GANs).',
     long_description=read_readme("README.md"),
     long_description_content_type='text/markdown',

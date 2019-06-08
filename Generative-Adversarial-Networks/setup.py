2fbb38c595b85fe4d78b1b0d765b8e2f9935bf06
chimera0 <ai-brain-lab@accel-brain.com>
2019-06-09 01:09:17 +0900

    Update version.

--- Generative-Adversarial-Networks/setup.py
+++ Generative-Adversarial-Networks/setup.py
@@ -12,7 +12,7 @@ def read_readme(file_name):
 
 setup(
     name='pygan',
-    version='1.0.3',
+    version='1.0.4',
     description='pygan is Python library to implement Generative Adversarial Networks and Adversarial Auto-Encoders.',
     long_description=read_readme("README.md"),
     long_description_content_type='text/markdown',

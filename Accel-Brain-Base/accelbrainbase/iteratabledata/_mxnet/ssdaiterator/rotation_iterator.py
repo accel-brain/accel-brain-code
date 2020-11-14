04d6bee85c65117f910795ede2526baf0318a6b7
accel-brain <info@accel-brain.co.jp>
2020-11-14 21:58:18 +0900

    Add iterator for SSDA.

---
+++ Accel-Brain-Base/accelbrainbase/iteratabledata/_mxnet/ssdaiterator/rotation_iterator.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from accelbrainbase.iteratabledata._mxnet.ssda_iterator import SSDAIterator
+import mxnet.ndarray as nd
+import numpy as np
+from abc import abstractmethod
+
+
+class RotationIterator(SSDAIterator):
+    '''
+    Iterator that draws from image files and generates `mxnet.ndarray`.
+
+    References:
+        - Ghifary, M., Kleijn, W. B., Zhang, M., Balduzzi, D., & Li, W. (2016, October). Deep reconstruction-classification networks for unsupervised domain adaptation. In European Conference on Computer Vision (pp. 597-613). Springer, Cham.
+        - Xu, J., Xiao, L., & López, A. M. (2019). Self-supervised domain adaptation for computer vision tasks. IEEE Access, 7, 156694-156706., p156698.
+    '''
+
+    def create_pretext_task_samples(self, target_domain_batch_arr):
+        '''
+        Create samples for pretext_task.
+
+        Args:
+            target_domain_batch_arr:    `nd.ndarray` of samples in target domain.
+        
+        Returns:
+            Tuple data. The shape is ...
+            - pretext_task samples.
+            - pretext_task labels.
+        '''
+        angle_list = []
+        for batch in range(target_domain_batch_arr.shape[0]):
+            angle_key = np.random.randint(low=0, high=4)
+            angle_arr = np.zeros(4)
+            angle_arr[angle_key] = 1
+            angle = 90 * angle_key
+            target_domain_batch_arr[batch] = self.__rotate(target_domain_batch_arr[batch], angle)
+
+            angle_list.append(angle_arr)
+
+        angle_arr = nd.ndarray.array(angle_list, ctx=pretext_arr.context)
+
+        return target_domain_batch_arr, angle_arr
+
+    def __rotate(self, arr, angle=0):
+        if angle == 0:
+            return arr
+        elif angle == 90:
+            arr = arr.transpose((0, 2, 1))
+            return arr
+        elif angle == 180:
+            arr = nd.flip(arr, axis=1)
+            return arr
+        elif angle == 270:
+            arr = arr.transpose((0, 2, 1))
+            arr = nd.flip(arr, axis=1)
+            return arr

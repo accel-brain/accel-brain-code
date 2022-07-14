8717d4e468f1adea63a8f0b81a584efd05f7f968
Accel Brain Co., Ltd <info@accel-brain.co.jp>
2022-07-14 13:18:51 +0900

    Update for interface.

--- Accel-Brain-Base/accelbrainbase/observabledata/_torch/attentionmodel/multiheadattentionmodel/self_attention_model.py
+++ Accel-Brain-Base/accelbrainbase/observabledata/_torch/attentionmodel/multiheadattentionmodel/self_attention_model.py
@@ -22,7 +22,7 @@ class SelfAttentionModel(MultiHeadAttentionModel):
 
     '''
 
-    def inference(self, observed_arr, memory_arr, mask=None):
+    def inference(self, observed_arr, mask=None):
         '''
         Inference samples drawn by `IteratableData.generate_inferenced_samples()`.
 
@@ -33,19 +33,18 @@ class SelfAttentionModel(MultiHeadAttentionModel):
         Returns:
             `mxnet.ndarray` of inferenced feature points.
         '''
-        return self.forward(observed_arr, observed_arr, mask)
+        return super().inference(observed_arr, observed_arr.detach(), mask)
 
-    def forward(self, x, m, mask=None):
+    def forward(self, x, mask=None):
         '''
         Forward with Gluon API.
 
         Args:
             F:      `mxnet.ndarray` or `mxnet.symbol`.
             x:      `mxnet.ndarray` of observed data points.
-            m:      `mxnet.ndarray` of observed data points. The shape is (batch_size, length of memory, depth).
             mask:   `mxnet.ndarray` of mask.
 
         Returns:
             `mxnet.ndarray` or `mxnet.symbol` of inferenced feature points.
         '''
-        return super().forward(x=x, m=x.detach(), mask=mask)
+        return super().forward(x, x.detach(), mask)
